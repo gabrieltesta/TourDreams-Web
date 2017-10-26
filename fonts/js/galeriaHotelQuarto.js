20ffaa575add90b8f58ff1e78f2afdb03ef6dd77
gabrieltesta <gabrieltesta>
2017-10-25 23:33:55 -0200

    2.29.1

--- fonts/js/galeriaHotelQuarto.js
+++
@@ -1,6 +0,0 @@
-//Arquivo contém funções para o funcionamento da galeria da página hotelQuarto.php
-
-//Função faz a troca de imagens na galeria
-function trocaImg(img){
-    $('#divImgGrande img').attr('src',img);
-}
