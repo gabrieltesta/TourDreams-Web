6aac6de07c9634f365a5597d8f6998dc00fbdd57
gabrieltesta <gabrieltesta>
2017-11-15 02:46:24 -0200

    2.44.1

--- views/parceiros/promocoes/promocoes_view.php
+++
@@ -1,6 +0,0 @@
-<section id="sectionp">
-    <ul class="rslides">
-        <li><img src="imagens/promocoes/promocao1.jpg" alt=""></li>
-        <li><img src="imagens/promocoes/promocao2.jpg" alt=""></li>
-    </ul>
-</section>
